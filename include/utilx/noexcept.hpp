84f36788cac1865f245091926513920033f39466
SirTobi <code.databyte@gmail.com>
2015-04-14 10:10:33 +0200

    added UTILX_NOEXCEPT

---
+++ include/utilx/noexcept.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef _UTILX_NOEXCEPT_HPP
+#define _UTILX_NOEXCEPT_HPP
+
+#ifdef _NOEXCEPT
+#	define UTILX_NOEXCEPT _NOEXCEPT
+#else
+#	define UTILX_NOEXCEPT noexcept
+#endif
+
+
+#endif
