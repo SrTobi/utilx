aad768c5efe49fbf64cabc8d28aa36693a17f554
SirTobi <code.databyte@gmail.com>
2015-04-24 00:03:59 +0200

    Added documentation for noexcept

--- include/utilx/noexcept.hpp
+++ include/utilx/noexcept.hpp
@@ -2,11 +2,29 @@
 #ifndef _UTILX_NOEXCEPT_HPP
 #define _UTILX_NOEXCEPT_HPP
 
+/**
+ *	\file 
+ *
+ *	\defgroup macros Macros
+ *	Contains helper macros.
+ *	
+ *	\ingroup macros
+ *	@{
+ */
+
+
+/**
+ *	\def UTILX_NOEXCEPT
+ *	Wraps noexcepts into a macro. This can be used to maintain compatibility for compilers that do not support noexcept.
+ *	
+ */
 #ifdef _NOEXCEPT
 #	define UTILX_NOEXCEPT _NOEXCEPT
 #else
 #	define UTILX_NOEXCEPT noexcept
 #endif
 
+
+/*! @} */
 
 #endif
