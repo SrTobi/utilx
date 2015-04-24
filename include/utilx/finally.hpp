62f0d7c6a731996f918f53ac767de325d273818d
SirTobi <code.databyte@gmail.com>
2015-04-24 02:23:48 +0200

    added docs for finally

--- include/utilx/finally.hpp
+++ include/utilx/finally.hpp
@@ -2,14 +2,61 @@
 #ifndef _UTILX_FINALLY_HPP
 #define _UTILX_FINALLY_HPP
 
+/** \file */
 
 #include <functional>
 #include "noexcept.hpp"
 
+
 namespace utilx {
+	/**
+	 *	\ingroup utils
+	 *	\brief A helper class to execute code, when exiting the scope.
+	 *	
+	 *	finally calls a given function, when the scope in which it was defined
+	 *	ends. In contrast to a function
+	 *	that is called at the end of a block directly, finally also gets called
+	 *	when the block is exited at an unexpected location (i.e. a exception was thrown).
+	 *	In the following example `Scope exited!` will be printed, even if `other_func()` throws an exception.
+	 *	
+	 *		void foo()
+	 *		{
+	 *			finally f = []() {
+	 *				std::cout << "Scope exited!" << std::endl;
+	 *			};
+	 *			
+	 *			...
+	 *			
+	 *			other_func();
+	 *			
+	 *			...
+	 *		}
+	 *		
+	 *	\note 
+	 *		Note that two or more finally will be executed in the inverse order as they where constructed.
+	 *		
+	 *			{
+	 *				finally b = ...;
+	 *				finally a = ...;
+	 *				
+	 *				// a will be executed
+	 *				// b will be executed
+	 *			}
+	 *		
+	 *
+	 *	\note
+	 *		Note that finally can neither be copied nor be moved.
+	 */
 	class finally
 	{
 	public:
+		/**
+		 *	\brief Constructs finally from a given function.
+		 *	
+		 *	The given function must be convertible to `std::function< void() >`.
+		 *
+		 *	\param _func	a handler that is called when finally gets destroyed.
+		 */
 		template<typename Func>
 		finally(Func _func)
 			: mFunctor(_func)
