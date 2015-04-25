0867ff7cc058fbae77e309d4afcc23f6bba0da5a
SirTobi <code.databyte@gmail.com>
2015-04-25 19:08:48 +0200

    added docs for make_unique

--- include/utilx/make_unique.hpp
+++ include/utilx/make_unique.hpp
@@ -2,6 +2,8 @@
 #ifndef _UTILX_MAKE_UNIQUE_HPP
 #define _UTILX_MAKE_UNIQUE_HPP
 
+/** \file */
+
 #include <memory>
 #include <type_traits>
 #include <utility>
@@ -44,6 +46,15 @@ namespace utilx {
 #if UTILX_FEATURES_HAS_MAKE_UNIQUE && !defined(DOXYGEN_DOCS)
 	using std::make_unique;
 #else
+	/**
+	 *	\ingroup utils
+	 *	\brief Constructs an object of type T and wraps it in a std::unique_ptr.
+	 *
+	 *	For more information see [std::make_unique](http://en.cppreference.com/w/cpp/memory/unique_ptr/make_unique).
+	 *	
+	 *	\exception std::bad_alloc
+	 *	\exception "Any exception" that is thrown by a constructor of *T*
+	 */
 	template <typename T, typename... Args>
 	std::unique_ptr<T> make_unique(Args&&... args) {
 		return detail::make_unique_helper<T>(std::is_array<T>(), std::forward<Args>(args)...);
